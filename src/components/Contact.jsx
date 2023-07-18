b4963b37e938a70abb8d9b3773442c6a4cb23c5a
Essa Azimi <azimiessa220@gmail.com>
2023-07-18 05:19:00 +0500

    changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,7 +27,7 @@ const Contact = () => {
                   azimiessa220@gmail.com
                 </span>
               </a> */}
-              <ButtonMailto label="Write me an E-Mail" mailto="mailto:azimiessa220@gmail.com" />
+              <ButtonMailto mailto="mailto:azimiessa220@gmail.com" />
             </p>
             <p className=''>
               <span className='text-[#eb5939]'>
